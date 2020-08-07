7fd0accaaf38415e21b2be5e366998629c3a28f0
Jovi De Croock <decroockjovi@gmail.com>
2020-08-07 16:00:06 +0200

    properly space images

--- src/areas/Skills.js
+++ src/areas/Skills.js
@@ -6,10 +6,15 @@ import scanner from '../assets/images/icons/3dscanner.png'
 import plan from '../assets/images/icons/plan.png'
 import pointcloud from '../assets/images/icons/pointcloud.png'
 
-const ImageWrapper = styled.img`
+const Gallery = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Image = styled.img`
   cursor: pointer;
-  width: 25%;
-  height: 25%;
+  width: 20%;
+  height: 20%;
 `
 
 const Skills = () => {
@@ -20,10 +25,13 @@ const Skills = () => {
     <section id="two">
       <div className="skills">
         <h2>{t('skills')}</h2>
-        <ImageWrapper alt="3D laser scanner" src={scanner}/>
-        <ImageWrapper alt="pointcloud" src={pointcloud} />
-        <ImageWrapper alt="plan of a building" src={plan} />
-        <ImageWrapper alt="3D model of building" src={drawing} />
+        <Gallery>
+          <Image alt="3D laser scanner" src={scanner}/>
+          <Image alt="pointcloud" src={pointcloud} />
+          <Image alt="plan of a building" src={plan} />
+          <Image alt="3D model of building" src={drawing} />
+        </Gallery>
+
       </div>
     </section>
   )
