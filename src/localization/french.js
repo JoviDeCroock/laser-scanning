5875eb99fd88774c1cd648496ae8db8fda6a3eb9
Jovi De Croock <decroockjovi@gmail.com>
2020-08-10 17:40:27 +0200

    add french

--- src/localization/french.js
+++ src/localization/french.js
@@ -8,38 +8,29 @@ export default {
     threeD: 'Numérisation et traitement 3D',
     siteDescription: "3D Laser Scanning",
     introduction: 'Introduction',
-    introCopy: `EDS Systems BV is een team van gedreven tekenaars met ervaring in de bouw, die zich
-    toegelegd hebben op digitale opmeting en verwerking van gebouwen, monumenten,
-    industriële installaties en infrastructuur. Deze opmeting kan al dan niet uitgevoerd worden in
-    samenwerking met een landmeter.`,
-    service: `EDS Systems BV kan U volgende mogelijke samenwerkingen aanbieden:`,
-    serviceMeasure: `Digitale opmeting en verwerking tot puntenwolk`,
-    servicePlan: `Digitale opmeting (inclusief verwerking tot puntenwolk) en verwerking tot plan`,
-    serviceModel: `Digitale opmeting (inclusief verwerking tot puntenwolk) en verwerking tot 3D of BIM model`,
-    serviceFull: `Verwerking van de door U aangeleverde puntenwolk tot plan, 3D of BIM model`,
-    laserScanner1: `3D laserscanning is een techniek waarbij op zeer korte tijd bestaande situaties uiterste precies en in
-    drie dimensies opgemeten worden. De laserscanner verricht niet enkel gedetailleerde metingen, maar
-    kan tevens panoramische beelden vastleggen. Hierdoor is deze techniek uiterst geschikt om de
-    complexiteit van een project te beheren aan de hand van exacte en betrouwbare 3D-voorstellingen van
-    de bestaande situatie (As-built). De opmeting vormt een geschikte basis voor nauwkeurige plannen en
-    modellen.`,
-    laserScanner2: `3D laserscanning kan toegepast worden voor opmetingen van gebouwen, monumenten, industriële
-    installaties, infrastructuur, en zoveel meer. Dit zowel voor de constructie voor een snelle en correcte
-    opmeting van de huidige situatie met een lage foutmarge en realistische panoramische beelden;
-    tijdens de constructie voor as-built-, leiding- en constructieplannen; en na de constructie voor as-built
-    en controleplannen.`,
-    pointcloud1: `Elk punt dat opgemeten wordt door de 3D laserscanner, wordt gedefinieerd door een uiterst
-    nauwkeurige x-, y-, z-positie. Al deze punten samen vormen een puntenwolk die de huidige
-    situatie nauwgezet weergeven. Deze puntenwolk vormt de geschikte basis voor de verwerking
-    tot gedetailleerde plannen en modellen.
-    De toevoeging van panoramische beelden maakt het mogelijk voor de gebruiker om een beter
-    inzicht te verkrijgen in de gescande situatie. Dit leidt tot een sterke vermindering van het aantal
-    plaats bezoeken en is tevens een handig visueel hulpmiddel bij overleg.`,
-    plan1: `De verworven puntenwolk dient als basis voor het uitwerken van inplantingsplannen,
-    grondplannen, detailplannen, verticale doorsnedes en gevelzichten. De graad van uitwerking
-    wordt op voorhand vastgelegd (i.e. detaillering van deuren, ramen, ornamenten,...).`,
-    model1: `De verworven puntenwolk dient als basis voor het uitwerken van driedimensionale modellen
-    en visualisaties. De graad van uitwerking wordt op voorhand vastgelegd (i.e. detaillering van
-    deuren, ramen, ornamenten,...).`
+    introCopy: `EDS Systems BV est une équipe de dessinateurs enthousiastes ayant une grande expérience dans le secteur de la construction.
+    Ils se sont spécialisés dans les prises de mesures digitales de bâtiments, de monuments, d’installations et d’infrastructures industrielles
+    ainsi que dans le traitement de ces données. Ces prises de mesures peuvent se faire avec ou sans l’assistance d’un géomètre.`,
+    service: `EDS Systems BV peut vous proposer les services suivants:`,
+    serviceMeasure: `Prises de mesures digitales et traitement vers nuage de points`,
+    servicePlan: `Prises de mesures digitales (traitement vers nuage de points inclus) et production d’un plan`,
+    serviceModel: `Prises de mesures digitales (traitement vers nuage de points inclus) et production d’un modèle 3D ou BIM`,
+    serviceFull: `Production d’un plan, modèle 3D ou BIM à partir d’un nuage de points fourni par le client`,
+    laserScanner1: `Le scanning laser 3D est une technique qui permet de mesurer une situation existante très rapidement et avec une grande précision.
+    Le scanner laser effectue des mesures précises et peut également enregistrer des vues panoramiques.
+    Cette technique est donc parfaitement adaptée à la gestion d’un projet complexe grâce à des représentations 3D fiables de la situation réelle (As-Built).
+    Les prises de mesures constituent  la base idéale pour les plans et les modèles.`,
+    laserScanner2: `Le scanning laser peut s’appliquer aux prises de mesures de bâtiments, de monuments, d’installations et d’infrastructures industrielles
+    et beaucoup plus encore. Elles peuvent se faire aussi bien avant la construction, afin d’effectuer des mesures rapides et correctes de la situation
+    initiale avec une marge d’erreur infime et  des images panoramiques réalistes; pendant la construction, pour les plans « as-built », de canalisations et de
+    construction; après la construction pour les plans « as-built » et les plans de contrôle.`,
+    pointcloud1: `Chaque point mesuré par le scanner 3D est défini avec grande précision par ses trois coordonnées  x, y, z. Tous ces points forment un nuage
+    de points qui traduisent parfaitement la situation réelle. Ce nuage de points constitue la base idéale pour la création de plans et de modèles détaillés.
+    La superposition de vues panoramiques permet à l’utilisateur d’avoir une meilleure lecture du rendu du scan. Cette possibilité permet de diminuer le nombre
+    de visites sur place en plus d’être un outil visuel pratique lors des réunions.`,
+    plan1: `Le nuage de points collecté sert de base au traitement des plans d’implantations, des plans d’étage, des plans de détails, des coupes verticales et
+    des vues de façades. Le niveau de détail souhaité est défini d’avance (i.e. représentation des portes, fenêtres, décorations,…).`,
+    model1: `Le nuage de points collecté sert de base au traitement des modèles 3D et des vues. Le niveau de détail souhaité est défini d’avance
+    (i.e. représentation des portes, fenêtres, décorations,…).`
   },
 }
