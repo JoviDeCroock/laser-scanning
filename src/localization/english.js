b438c307c7a7b95730129982d137d5e89082cc1b
Jovi De Croock <decroockjovi@gmail.com>
2020-10-08 21:18:52 +0200

    last one

--- src/localization/english.js
+++ src/localization/english.js
@@ -18,18 +18,16 @@ export default {
     servicePlan: `Digital measurement (including processing to pointcloud) and processing to plan`,
     serviceModel: `Digital measurement (including processing to pointcloud) and processing to a 3D or BIM model`,
     serviceFull: `Processing the by you delivered pointcloud to plan, 3D or BIM model`,
-    introCopy: `EDS Systems is a team of passionate designers with a lot of experience in construction, they specialise in 
-    digital measurement and processing of buildings, monuments, industrial installations and infrastructure. 
+    introCopy: `EDS Systems is a team of passionate designers with a lot of experience in construction, they specialise in
+    digital measurement and processing of buildings, monuments, industrial installations and infrastructure.
     This measurement could be performed together with a surveyor.`,
     laserScanner1: `3D laserscanning is a technique that allows us to measure buildings, ... in 3D and in a very short amount of time. 
-    The scanner does not only perform detailed measurements, but it also makes panoramic images. That's why this technique is suited 
-    for the complexity of managing a project with an exact and reliable 3D representation of the current situation. 
+    The scanner does not only perform detailed measurements, but it also makes panoramic images. That's why this technique is suited
+    for the complexity of managing a project with an exact and reliable 3D representation of the current situation.
     The measurement is a suitable base for precise drawings and models.`,
-    laserScanner2: `3D laserscanning kan toegepast worden voor opmetingen van gebouwen, monumenten, industriële
-    installaties, infrastructuur, en zoveel meer. Dit zowel voor de constructie voor een snelle en correcte
-    opmeting van de huidige situatie met een lage foutmarge en realistische panoramische beelden;
-    tijdens de constructie voor as-built-, leiding- en constructieplannen; en na de constructie voor as-built
-    en controleplannen.`,
+    laserScanner2: `3D laserscanning can be applied for measuring buildings, monuments, industrial installations, infrastructure and so much more.
+    This can be throughout the process of a building, before construction for a fast and correct measurement of the current situation with a low margin of error and realistic panoramic images;
+    during the construction for as-built-, drainage- and contructionplans; and after the construction for as-built and control plans.`,
     pointcloud1: `Elk punt dat opgemeten wordt door de 3D laserscanner, wordt gedefinieerd door een uiterst
     nauwkeurige x-, y-, z-positie. Al deze punten samen vormen een puntenwolk die de huidige
     situatie nauwgezet weergeven. Deze puntenwolk vormt de geschikte basis voor de verwerking
@@ -37,11 +35,9 @@ export default {
     De toevoeging van panoramische beelden maakt het mogelijk voor de gebruiker om een beter
     inzicht te verkrijgen in de gescande situatie. Dit leidt tot een sterke vermindering van het aantal
     plaats bezoeken en is tevens een handig visueel hulpmiddel bij overleg.`,
-    plan1: `De verworven puntenwolk dient als basis voor het uitwerken van inplantingsplannen,
-    grondplannen, detailplannen, verticale doorsnedes en gevelzichten. De graad van uitwerking
-    wordt op voorhand vastgelegd (i.e. detaillering van deuren, ramen, ornamenten,...).`,
-    model1: `De verworven puntenwolk dient als basis voor het uitwerken van driedimensionale modellen
-    en visualisaties. De graad van uitwerking wordt op voorhand vastgelegd (i.e. detaillering van
-    deuren, ramen, ornamenten,...).`
+    plan1: `The acquired pointcloud will serve as a base for the creation of the siting plans, ground plans, detail plans, vertical intersections and facade views.
+    the grade of detail will be discussed in advance (i.e. details of doors, windows, ornaments,...).`,
+    model1: `The acquired pointcloud will serve as a base for the creation of our three dimensional models
+    and visualizations. The grade of detail will be discussed in advance (i.e. details of doors, windows, ornaments,...).`
   },
 }
