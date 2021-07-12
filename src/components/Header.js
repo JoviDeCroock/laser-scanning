ad59fa37af7b952d1cb2c7d656685f79a36a3edd
Jovi De Croock <decroockjovi@gmail.com>
2021-07-12 12:01:17 +0200

    add contact link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
 
 import Footer from './Footer'
 import avatar from '../assets/images/website-icon.png'
 import LanguageSelector from './LanguageSelector'
+
+const ContactLink = styled.a`
+  border: 0;
+  color: rgb(232, 230, 227);
+  cursor: pointer;
+  text-decoration: none;
+  width: unset !important;
+`;
 
 const Header = ({ language, setLanguage }) => {
   const { t } = useTranslation()
@@ -22,6 +31,7 @@ const Header = ({ language, setLanguage }) => {
         </p>
         <LanguageSelector language={language} setLanguage={setLanguage} />
       </div>
+      <ContactLink href="#four">Contact</ContactLink>
       <Footer />
     </header>
   )
